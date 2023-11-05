d043762dcbdf758697798f928123002674e5dec6
mmalcolm02 <mmalcolm02@qub.ac.uk>
2023-11-05 17:49:24 +0000

    Difficulty Buttons
    Error with the StartGame() routine?!

--- Clicky Mouse/Assets/Scripts/GameManager.cs
+++ Clicky Mouse/Assets/Scripts/GameManager.cs
@@ -2,20 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public float spawnRate = 1.0f;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
     private int score;
     public List<GameObject> targets;
+
+    public bool isGameActive = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnTargets());
-        score = 0;
-        UpdateScore(0);
+        
+
+
     }
 
     // Update is called once per frame
@@ -24,9 +30,18 @@ public class GameManager : MonoBehaviour
         
     }
 
+    public void StartGame()
+    {
+        isGameActive = true;
+        score = 0;
+
+        StartCoroutine(SpawnTargets());
+        UpdateScore(0);
+    }
+
     IEnumerator SpawnTargets()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
@@ -39,6 +54,18 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        isGameActive = false;
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
 }
--- Clicky Mouse/Assets/Scripts/TargetObjects.cs
+++ Clicky Mouse/Assets/Scripts/TargetObjects.cs
@@ -42,14 +42,24 @@ public class TargetObjects : MonoBehaviour
 
     private void OnMouseDown()
     {
-        Destroy(gameObject);
-        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
-        gameManager.UpdateScore(pointValue);
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
+       
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+
+               
     }
 
     Vector3 AddForce()
